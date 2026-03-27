17d961e95eb5878144717340fecdb869eeb1b0c5
GiorgiaFormicola <giorgia.formicola97@gmail.com>
2026-03-27 09:29:48 +0100

    Initial commit

---
+++ src/main/java/GiorgiaFormicola/Application.java
@@ -0,0 +1,17 @@
+package GiorgiaFormicola;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class Application {
+
+    private static final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("events_management_pu");
+
+    public static void main(String[] args) {
+
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        System.out.println("Hello World!");
+    }
+}
